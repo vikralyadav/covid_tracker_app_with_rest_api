947a388f3fb761a11b1daa3a5a7702e85e9b4b32
Bittu <yadavvikral2003@gmail.com>
2024-04-28 11:13:53 +0530

    first commit

---
+++ lib/View/detail_screen.dart
@@ -0,0 +1,65 @@
+
+
+import 'package:covid_tracker/View/world_states.dart';
+import 'package:flutter/material.dart';
+
+class DetailScreen extends StatefulWidget {
+  String image;
+  String name;
+  int totalCases, totalDeaths, totalRecovered, active, critical, todayRecovered, test;
+   DetailScreen({
+    required this.image,
+    required this.name,
+    required this.totalCases,
+    required this.totalDeaths,
+    required this.totalRecovered,
+    required this.active,
+    required this.critical,
+    required this.todayRecovered,
+    required this.test,
+
+    
+    });
+
+  @override
+  State<DetailScreen> createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.name),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Stack(
+            alignment: Alignment.topCenter,
+
+            children: [
+              Padding(
+                padding: EdgeInsetsDirectional.only(top: MediaQuery.of(context).size.height*0.67),
+                child: Card(
+                  child: Column(
+                    children: [
+                      SizedBox(height: MediaQuery.of(context).size.height*0.6,),
+                      ReusableRow(title: 'Cases', value: widget.totalCases.toString()),
+                       ReusableRow(title: 'Recovered', value: widget.totalRecovered.toString()),
+                
+                    ],
+                  ),
+                ),
+              ),
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: NetworkImage(widget.image),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
